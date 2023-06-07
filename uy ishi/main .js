7d28444621a56c16ee3b80d040c23767b2be015d
Shohruhxon09 <127945777+Shohruhxon09@users.noreply.github.com>
2023-06-07 23:48:57 +0500

    Add files via upload

---
+++ uy ishi/main .js
@@ -0,0 +1,18 @@
+const malumot = {
+    name:"Ibrohimjon",
+    famil: "Muhammad",
+    tel: 998936677559,
+    pasport: "AD098",
+    manzil: {
+        dom:98,
+        padez:78,
+    street:"uabekistan"
+    }
+}
+console.log(malumot.manzil.street);
+for(let i =0; i <=100; i++) {
+    if (i === 50) {
+        continue
+    }
+    console.log(i);
+}
